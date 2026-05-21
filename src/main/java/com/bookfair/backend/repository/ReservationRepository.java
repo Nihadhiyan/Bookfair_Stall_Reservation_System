37586cbfa2515c275cc4ed44fb3bca61855fc001
Mohamed Nihadh <nihath854@gmail.com>
2026-05-21 12:38:05 +0530

    refactor: removed unwanted file imports

--- src/main/java/com/bookfair/backend/repository/ReservationRepository.java
+++ src/main/java/com/bookfair/backend/repository/ReservationRepository.java
@@ -4,8 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.bookfair.backend.model.Reservation;
-import com.bookfair.backend.model.Reservation.ReservationStatus;
-import com.bookfair.backend.model.Stall;
 
 import java.time.LocalDate;
 import java.util.List;
--- src/main/java/com/bookfair/backend/repository/StallRepository.java
+++ src/main/java/com/bookfair/backend/repository/StallRepository.java
@@ -1,7 +1,6 @@
 package com.bookfair.backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.bookfair.backend.model.Stall;

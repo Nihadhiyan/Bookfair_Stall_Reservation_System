4d8a0f92705d61f22bac94b432a2eb8f797f34b5
Mohamed Nihadh <nihath854@gmail.com>
2026-06-16 21:42:55 +0530

    feat(user): add emailVerified flag to User entity and create ResetPasswordRequest DTO

---
+++ src/main/java/com/bookfair/backend/dto/auth/request/ResetPasswordRequest.java
@@ -0,0 +1,21 @@
+package com.bookfair.backend.dto.auth.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResetPasswordRequest {
+    @NotBlank(message = "Reset token must not be blank")
+    private String resetToken;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, max = 100, message = "Password must be between 8 and 100 characters")
+    private String newPassword;
+}

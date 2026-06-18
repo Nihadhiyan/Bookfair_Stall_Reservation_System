62d4ea4ab2e2e27854219d33343a77ec0f0a3ef5
Mohamed Nihadh <nihath854@gmail.com>
2026-06-18 20:20:17 +0530

    feat(user): add dedicated role update payload and implement pagination queries

---
+++ src/main/java/com/bookfair/backend/dto/user/request/UpdateUserRoleRequest.java
@@ -0,0 +1,20 @@
+package com.bookfair.backend.dto.user.request;
+
+import com.bookfair.backend.model.User.Role;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UpdateUserRoleRequest {
+
+    @NotNull(message = "Role is required")
+    private Role role;
+
+}
